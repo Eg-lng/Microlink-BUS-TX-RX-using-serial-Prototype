05874a1a137816d679c3177e0508db81f328e4b9
Eg.lng <lechimeur35@gmail.com>
2025-11-12 23:30:20 +0100

    Update TX_PROT_V1_4.ino with comments and formatting
    NC

--- TX_PROT_V1_4.ino
+++ TX_PROT_V1_4.ino
@@ -1,7 +1,14 @@
+// Project : Microlink BUS
+// Microlink BUS Transmitter Prototype using serial
+// Made by eg.lng / Follow me on Instagram @eg.lng , all right reserved, project KATZ systemes 
+// PINOUT for arduino nano
+// V1.4 / Last modification : 12/11/2025
+
+
 #include <Arduino.h>
 
 const int DATAOUT = 5; // PIN for the DATA
-const int CLKOUT = 6;
+const int CLKOUT = 6;  // PIN for the CLK
 
 String commande = "";
 
@@ -27,10 +34,10 @@ void bit0()
     delayMicroseconds(40); // 40 microsecondes
 }
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600);        // Serial
     pinMode(DATAOUT,OUTPUT); // pin data 
     pinMode(CLKOUT,OUTPUT); // pin clock 
-    Serial.println("ready");
+    Serial.println(" TX ready "); // Serial ready
 }
 
 void loop(){
@@ -39,7 +46,7 @@ void loop(){
     commande = Serial.readStringUntil('\n');
     commande.trim(); 
 
-    if (commande.equalsIgnoreCase("A")) {
+    if (commande.equalsIgnoreCase("A")) { // random test you need to recieve (101 ... 10)
       bit1();
       bit0();
       bit1();
@@ -74,7 +81,7 @@ void loop(){
       bit0();
       Serial.println(" 1 ");
     } 
-    else if (commande.equalsIgnoreCase("Z")) {
+    else if (commande.equalsIgnoreCase("Z")) { // random test you need to recieve (000 ... 000)
       bit0(); 
       bit0();
       bit0();
@@ -109,7 +116,7 @@ void loop(){
       bit0();
       Serial.println(" 0 ");
     } 
-  else if (commande.equalsIgnoreCase("E")) {
+  else if (commande.equalsIgnoreCase("E")) { // random test you need to recieve nothing
       bit0(); 
       bit0();
       bit0();
@@ -124,7 +131,7 @@ void loop(){
 
       Serial.println(" ERROR ");
     } 
-   else if (commande.equalsIgnoreCase("R")) {
+   else if (commande.equalsIgnoreCase("R")) { //  Still in devloppement !!random test you need to recieve (000 ... 01 )
       bit0(); 
       bit0();
       bit0();
@@ -161,7 +168,7 @@ void loop(){
       bit1();
       Serial.println(" OVERFLOW ");
     } 
-    else if (commande.equalsIgnoreCase("T")) {
+    else if (commande.equalsIgnoreCase("T")) { // random test you need to recieve (111 ... 111)
       bit1();
       bit1();
       bit1();
